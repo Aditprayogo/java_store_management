1ba45bf4514ae13417eb52744da67ecdac1b7ce9
Aditprayogo <adit.ihzar@gmail.com>
2019-12-09 19:30:12 +0700

    adding view for bukti pesan and make model for bukti pesan

---
+++ src/Model/BuktiPesan.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class BuktiPesan {
+    private String nobp;
+    private String tglbp;
+    private Integer kodeplg;
+    private String namaplg;
+    private Integer kodekategori;
+    private String namakategori;
+    private String kodebarang;
+    private String namabarang;
+
+    public String getNobp() {
+        return nobp;
+    }
+
+    public void setNobp(String nobp) {
+        this.nobp = nobp;
+    }
+
+    public String getTglbp() {
+        return tglbp;
+    }
+
+    public void setTglbp(String tglbp) {
+        this.tglbp = tglbp;
+    }
+
+    public Integer getKodeplg() {
+        return kodeplg;
+    }
+
+    public void setKodeplg(Integer kodeplg) {
+        this.kodeplg = kodeplg;
+    }
+
+    public String getNamaplg() {
+        return namaplg;
+    }
+
+    public void setNamaplg(String namaplg) {
+        this.namaplg = namaplg;
+    }
+
+    public Integer getKodekategori() {
+        return kodekategori;
+    }
+
+    public void setKodekategori(Integer kodekategori) {
+        this.kodekategori = kodekategori;
+    }
+
+    public String getNamakategori() {
+        return namakategori;
+    }
+
+    public void setNamakategori(String namakategori) {
+        this.namakategori = namakategori;
+    }
+
+    public String getKodebarang() {
+        return kodebarang;
+    }
+
+    public void setKodebarang(String kodebarang) {
+        this.kodebarang = kodebarang;
+    }
+
+    public String getNamabarang() {
+        return namabarang;
+    }
+
+    public void setNamabarang(String namabarang) {
+        this.namabarang = namabarang;
+    }
+
+    public Integer getHarga() {
+        return harga;
+    }
+
+    public void setHarga(Integer harga) {
+        this.harga = harga;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public Integer getStok() {
+        return stok;
+    }
+
+    public void setStok(Integer stok) {
+        this.stok = stok;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+    private Integer harga;
+    private Integer qty;
+    private Integer stok;
+    private Integer total;
+    
+    
+}
